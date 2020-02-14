39444f58fb23762459b40858274439e7807213d7
Lindsay Kelley <lmkelle3@Lindsays-MacBook-Air.local>
2020-02-13 18:45:33 -0700

    built claim model

--- src/main/java/com/capstone/claims/Claim.java
+++ src/main/java/com/capstone/claims/Claim.java
@@ -18,27 +18,90 @@ public class Claim {
     private int id;
 
     @Column
-    private String name;
+    private int userId;
 
     @Column
-    private String messages;
+    private String lossCategory;
 
     @Column
-    private int payments;
+    private String lossDescription;
+
+    @Column
+    private String DOL;
+
+    @Column
+    private String policyType;
+
+    @Column
+    private String itemType;
+
+    @Column
+    private String jurisdiction;
+
+    @Column
+    private String caseNumber;
+
+    @Column
+    private String dateFiled;
+
+    @Column
+    private String havePR;
+
+    @Column
+    private String claimValue;
+
+    @Column
+    private int paymentPreference;
 
     public int getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public int getUserId() {
+        return userId;
     }
 
-    public String getMessages() {
-        return messages;
+    public String getLossCategory() {
+        return lossCategory;
+    };
+
+    public String getLossDescription() {
+        return lossDescription;
+    };
+
+    public String getDOL() {
+        return DOL;
+    };
+
+    public String getPolicyType() {
+        return policyType;
+    };
+
+    public String getItemType() {
+        return itemType;
     }
 
-    public int getPayments() {
-        return payments;
+    public String getJurisdiction() {
+        return jurisdiction;
+    }
+
+    public String getCaseNumber() {
+        return caseNumber;
+    }
+
+    public String getDateFiled() {
+        return dateFiled;
+    }
+
+    public String getHavePR() {
+        return havePR;
+    }
+
+    public String getClaimValue() {
+        return claimValue;
+    }
+
+    public int getPaymentPreference() {
+        return paymentPreference;
     }
 }
